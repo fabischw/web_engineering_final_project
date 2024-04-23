448913e4adb64c9ac2b6496c20c4125996aaa723
Johannes Lingk <johannes.lingk@gmail.com>
2024-04-23 14:57:38 +0200

    Added index.js

---
+++ app/js/index.js
@@ -0,0 +1,31 @@
+/*
+Usage:  $(<query selector>) -> HTMLElement
+        $$(<query selector>) -> HTMLElements[]
+
+Example:    $("div.class-name")
+*/
+const $ = query => document.querySelector(query)
+const $$ = query => Array.from(document.querySelectorAll(query))
+
+/*
+Add Event listener to all elements matching css selector.
+*/
+const $on = (element, event, func) => {
+    Array.isArray(element)
+        ? element.forEach(arrayElement => $on(arrayElement, event, func))
+        : element.addEventListener(event, func)
+    return element
+}
+
+/*
+Handle bars integrations script.
+*/
+const render = (data) => {
+    const templates = $$('[type="text/x-handlebars-template"]')
+
+    for (const source of templates) {
+        const template = Handlebars.compile(source.innerHTML)
+        const target = source.nextElementSibling
+        target.innerHTML = template(data)
+    }
+}
