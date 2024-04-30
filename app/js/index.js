165670c00c50250d0e8269bf72f91cf2981d275f
Johannes Lingk <johannes.lingk@gmail.com>
2024-04-30 17:25:33 +0200

    Added cards, started dynamic loading.

--- app/js/index.js
+++ app/js/index.js
@@ -20,14 +20,27 @@ const $on = (element, event, func) => {
 /*
 Handle bars integrations script.
 */
-const render = (data) => {
+const render = async (pizzen) => {
     const templates = $$('[type="text/x-handlebars-template"]')
-
+  
     for (const source of templates) {
-        const template = Handlebars.compile(source.innerHTML)
-        const target = source.nextElementSibling
-        target.innerHTML = template(data)
+      await loadPartials(source)
+      const template = Handlebars.compile(source.innerHTML)
+      const target = source.parentElement
+      target.insertAdjacentHTML('beforeend', template(pizzen))
     }
+  }
+
+async function loadPartials(source) {
+const partialNames = source.innerText.match(/(?<={{>)(.*?)(?=\s|}})/g)
+if (partialNames) {
+    for (let name of partialNames) {
+    name = name.trim()
+    const fileName = name + '.html'
+    const partialCode = await fetch(fileName).then(response => response.text())
+    Handlebars.registerPartial(name, partialCode)
+    }
+}
 }
 
 
@@ -40,3 +53,14 @@ document.addEventListener('scroll', (event) => {
     var coords =  -(scrolled * 0.3) + 'px';
     parallax_container.style.top = coords;
 });
+
+handlebars_data = {
+    satellites: [
+        {
+            name: "First"
+        }, 
+        {
+            name: "Second"
+        }
+    ]
+}
