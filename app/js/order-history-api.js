1c644e3827f93b0d4ddd1e88f0d3bb1d31b84cae
Walz, Robert <rbrt.walz@gmail.com>
2024-05-28 16:27:11 +0200

    added more comments

--- app/js/order-history-api.js
+++ app/js/order-history-api.js
@@ -1,45 +1,66 @@
-// [order1, order2]
+/*
+[order1, order2, ..., orderN]
 
-// order: {
-//     cart: {
-//         item: {
-//             name: string,
-//             price: number,
-//             quantity: number
-//         }
-//     }, 
-//     quantity: number,
-//     billing: {
-//         first_name, 
-//         last_name, 
-//         email: string, 
-//         phone: string,
-//         company: string,
-//         address: {
-//             country: string,
-//             postcode: string,
-//             city: string,
-//             street: string,
-//             nr: number,
-//             address-addition: string
-//         }
-//     }
-// }
+This is what an order item looks like.
 
+order: {
+    cart: {
+        item: {
+            name: string,
+            price: number,
+            quantity: number
+        }
+    }, 
+    billing: {
+        firstName, 
+        lastName, 
+        email: string, 
+        phone: string,
+        company: string,
+        address: {
+            country: string,
+            postcode: string,
+            city: string,
+            street: string,
+            nr: number,
+            addressAddition: string
+    },
+    date: "DD.MM.YYYY"
+    }
+}
+
+*/
+
+
+
+// init oder history on document load
 document.addEventListener('DOMContentLoaded', function () {
     if (!window.localStorage.getItem("order-history")) {
         setOrderHistoryInLocalStorage([])
     }
 })
 
+/**
+ * set the array of orders to the order-history key in the local storage    
+ * @param {order[]} items 
+ */
+
 function setOrderHistoryInLocalStorage(items) {
     window.localStorage.setItem("order-history", JSON.stringify(items))
 }
 
+/**
+ * gets the array of past orders from the order-history key in local storage
+ * @returns order[]
+ */
 function getOrderHistoryFromLocalStorage() {
     return JSON.parse(window.localStorage.getItem("order-history"))
 }
 
+/**
+ * adds an order item to the array in the local storage
+ * @param {order} item 
+ */
 function addItemToOrderHistory(item) {
     let items = getOrderHistoryFromLocalStorage()
     items.push(item)
