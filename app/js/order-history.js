ef547d98133d686066c5fb189dda80a7c69e4fec
Walz, Robert <rbrt.walz@gmail.com>
2024-05-29 11:15:16 +0200

    finished dynamic loading

--- app/js/order-history.js
+++ app/js/order-history.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    render({ orders: window.order_history_api.getOrderHistoryFromLocalStorage() })
+    orderHistory = window.order_history_api.getOrderHistoryFromLocalStorage()
+    calcTotalPriceForEachOrder(orderHistory)
+    console.log(orderHistory)
+    render({orders: orderHistory})
 })
+
+function calcTotalPriceForEachOrder(orderHistory) {
+    orderHistory.forEach(order => {
+        order.totalPrice = 0
+        order.cart.forEach(item => {
+            order.totalPrice += item.price * item.quantity
+        })
+    })
+}
