8bb6ec3355148cdbee7d074bbdd74bc823b9000f
Walz, Robert <rbrt.walz@gmail.com>
2024-05-29 18:18:40 +0200

    calcs total price now

--- app/js/order-history-details.js
+++ app/js/order-history-details.js
@@ -1,8 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const orderId = parseInt(urlParams.get('id'))
-        render({
-            order: window.order_history_api.getOrderById(orderId)
-        })
+    const urlParams = new URLSearchParams(queryString);
+    const orderId = parseInt(urlParams.get('id'))
+    currentOrder = window.order_history_api.getOrderById(orderId)
+    if (currentOrder) {
+        calcTotalPriceForOrder(currentOrder)
+    }
+    render({
+        order: currentOrder
+    })
 })
+
+function calcTotalPriceForOrder(order) {
+    order.totalPrice = 0
+    order.cart.forEach(item => {
+        order.totalPrice += item.price * item.quantity
+    })
+}
