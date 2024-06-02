f9d67dbf5442c5c1b2b908bf15152f589f7d0687
LeMixer <78697721+LeMixer@users.noreply.github.com>
2024-06-02 18:38:48 +0200

    86 select rocket page (#93)
    * added launcher api
    
    * added global export
    
    * handlebars dynamic rendering
    
    * selecting works now
    
    * started adding functionality to buttons
    
    * site now works
    
    * added comma seperator to price
    
    * edited subtitle

---
+++ app/js/launcher-api.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (!window.localStorage.getItem("launcher-catalogue")) {
+        setLauncherCatalogueInLocalStorage(launchers)
+    }
+})
+
+/**
+ * set the launcher_catalogue to the launcher-catalogue key in the local storage    
+ * @param {object} catalogue 
+ */
+
+function setLauncherCatalogueInLocalStorage(catalogue) {
+    window.localStorage.setItem("launcher-catalogue", JSON.stringify(catalogue))
+}
+
+/**
+ * gets the launchers from local storage
+ * @returns launchers
+ */
+function getLauncherCatalogueFromLocalStorage() {
+    return JSON.parse(window.localStorage.getItem("launcher-catalogue"))
+}
+
+
+/**
+ * 
+ * @param {number} launcher_id 
+ * @returns {object} launcher as specified in launcher.js
+ */
+function getLauncherById(launcher_id) {   
+    return getLauncherCatalogueFromLocalStorage().find(x => x.id === launcher_id)
+}
+
+window.launcher_catalogue_api = {
+    setLauncherCatalogueInLocalStorage, 
+    getLauncherCatalogueFromLocalStorage,
+    getLauncherById
+}
